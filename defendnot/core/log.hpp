5ba4b7ee0ecaa505ed62c49ae6579cf95475a1b1
mranonymous011000001 <mranonymous011000001@gmail.com>
2025-05-25 15:14:25 +0530

    Update log.hpp

--- defendnot/core/log.hpp
+++ defendnot/core/log.hpp
@@ -1,19 +1,20 @@
+// In core/log.hpp
 #pragma once
-#include "shared/ctx.hpp"
-#include "shared/util.hpp"
+// REMOVE: #include "shared/ctx.hpp"
+#include "shared/util.hpp" // For alloc_console
 #include <print>
 #include <thread>
-
 #include <Windows.h>
 
 namespace defendnot {
+    constexpr bool HARDCODED_LOG_VERBOSE = true; // Or false
+
     template <typename... TArgs>
     void logln(const std::format_string<TArgs...> fmt, TArgs... args) noexcept {
-        if (!shared::ctx.verbose) [[likely]] {
+        if (!HARDCODED_LOG_VERBOSE) { // Use a local hardcoded value
             return;
         }
-
-        shared::alloc_console();
+        shared::alloc_console(); // shared::alloc_console itself doesn't depend on ctx
         std::println(stdout, fmt, std::forward<TArgs>(args)...);
     }
-} // namespace defendnot
+}
